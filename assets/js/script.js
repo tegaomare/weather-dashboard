650a086414e138e0d9be50b106cbc2f0c44c72a9
Tegaomare <tegaomare@oghenetegasimac.mynetworksettings.com>
2023-05-30 15:04:11 -0400

    added user interraction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,26 @@
 //INDEPENDENCES
 let searchForm = document.getElementById("search-form");
 let cityInput = document.getElementById("city-input");
+let searchBtn = document.getElementById("Search-button");
 
 //DATA
+let apiKey = "a156ce36549069ae356f11172fc650e1";
+let cityName = cityInput;
+
 //USER INTERRACTION
+searchBtn.addEventListener("click", displayCurrentWeather);
 //FUNCTION
 let requestUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=a156ce36549069ae356f11172fc650e1";
+  "https://api.openweathermap.org/data/2.5/weather?q=staten island&appid=a156ce36549069ae356f11172fc650e1";
 let responseText = document.getElementById("response-text");
 
 function getApi(requestUrl) {
   fetch(requestUrl).then(function (response) {
     console.log(response);
-    if (response.status === 200) {
+    /*  if (response.status === 200) {
       responseText.textContent = response.status;
     }
-    return response.json();
+    return response.json();*/
   });
 }
 
