df068ee3588f28bd9b7dce1683f1ec71a29a7b66
Tegaomare <tegaomare@oghenetegasimac.mynetworksettings.com>
2023-05-30 17:00:29 -0400

    added both url fetch request

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,26 +2,54 @@
 let searchForm = document.getElementById("search-form");
 let cityInput = document.getElementById("city-input");
 let searchBtn = document.getElementById("Search-button");
-
-//DATA
+let city = "staten island";
 let apiKey = "a156ce36549069ae356f11172fc650e1";
+let requestUrl2 =
+  "https://api.openweathermap.org/data/2.5/forecast?q=" +
+  city +
+  "&appid=" +
+  apiKey +
+  "&units=imperial";
+let requestUrl =
+  "https://api.openweathermap.org/data/2.5/weather?q=" +
+  city +
+  "&appid=a156ce36549069ae356f11172fc650e1&units=imperial";
+//DATA
 let cityName = cityInput;
 
 //USER INTERRACTION
-searchBtn.addEventListener("click", displayCurrentWeather);
+searchBtn.addEventListener("click", getApi);
 //FUNCTION
-let requestUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q=staten island&appid=a156ce36549069ae356f11172fc650e1";
 let responseText = document.getElementById("response-text");
 
-function getApi(requestUrl) {
-  fetch(requestUrl).then(function (response) {
-    console.log(response);
-    /*  if (response.status === 200) {
+function getApi() {
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+  /*  if (response.status === 200) {
       responseText.textContent = response.status;
     }
     return response.json();*/
-  });
+
+  getApi2();
+}
+function getApi2() {
+  fetch(requestUrl2)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+  /*  if (response.status === 200) {
+      responseText.textContent = response.status;
+    }
+    return response.json();*/
 }
 
-getApi(requestUrl);
+getApi();
+//for (i=0; i < somearray.lenght; i+=8) for url2
